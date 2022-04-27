144d934b28ce9ab504d7e666ee99a4c205396bf7
Jyoti <jyotivangari9@gmail.com>
2022-04-27 14:59:49 +0530

    FirstCommit

---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class ClassTest
+{
+ 
+	@Test
+	public void MavenTest1()
+	{
+		System.out.println("MavenTest122222222221");
+		
+	}
+	@Test
+	public void MavenTest11()
+	{
+		System.out.println("MavenTest11111222222211");
+		
+	}
+	
+}
---
+++ src/test/java/MavenPack/ClassTest.java
@@ -0,0 +1,20 @@
+package MavenPack;
+import org.testng.annotations.Test;
+
+public class ClassTest
+{
+ 
+	@Test
+	public void MavenTest1()
+	{
+		System.out.println("MavenTest1");
+		
+	}
+	@Test
+	public void MavenTest11()
+	{
+		System.out.println("MavenTest2");
+		
+	}
+	
+}
